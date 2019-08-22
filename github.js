52a052b09174b47407a6f212154de1de19ec3929
maarten079 <m.moerland.92@gmail.com>
2019-08-22 14:09:33 +0200

    fin

---
+++ github.js
@@ -0,0 +1,32 @@
+class Github {
+  constructor() {
+    this.client_id = "e76ee4db874f5ca9db0b";
+    this.client_secret = "c54cc3e2f502ccd3f986446e476d200473067a03";
+    this.repos_count = 5;
+    this.repos_sort = "created: asc";
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${
+        this.client_id
+      }&client_secret=${this.client_secret}`
+    );
+
+    const reposResponse = await fetch(
+      `https://api.github.com/users/${user}/repos?per_page=${
+        this.repos_count
+      }&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${
+        this.client_secret
+      }`
+    );
+
+    const profile = await profileResponse.json();
+    const repos = await reposResponse.json();
+
+    return {
+      profile,
+      repos
+    };
+  }
+}
